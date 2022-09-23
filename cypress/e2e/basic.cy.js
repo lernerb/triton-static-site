1f2b6fffafe4cb7e01a4459e543200662f61a00b
Brandon Lerner <lernerbrandon@gmail.com>
2022-09-23 13:43:32 -0400

    [Test] Fix

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -2,11 +2,6 @@ describe('sample test', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-
-  it('displays the resources text', () => {
-    cy.get('h1')
-    .contains('Welcome to my app!');
-  })
   it('renders the Netlify logo image', () => {
     cy.get('img')
     .should('be.visible')
